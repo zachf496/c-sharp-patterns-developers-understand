75c60b03f0c1424938017a698f25352c7d2653e5
Zachary <zacharyf496@gmail.com>
2016-03-10 13:44:17 -0500

    Init

---
+++ Singleton/MySingleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class MySingleton
+    {
+        private static volatile MySingleton _instance;
+        private static object _padLock = new Object();
+
+        public string PropertyOne;
+
+        private MySingleton()
+        {
+        }
+
+        public static MySingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_padLock)
+                    {
+                        if (_instance == null)
+                        {
+                            Console.WriteLine("Initializing singleton...");
+
+                            _instance = new MySingleton();
+
+                            _instance.PropertyOne = "Hello World!";
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public void Clear()
+        {
+            Console.WriteLine("Clearing singleton...");
+            _instance = null;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MySingleton.Instance.PropertyOne); //hello world
+
+            Console.WriteLine(MySingleton.Instance.PropertyOne); //hello world
+
+            MySingleton.Instance.Clear(); //will require a re-initialization on the next usage
+
+            Console.WriteLine(MySingleton.Instance.PropertyOne); //hello world
+        }
+    }
+}
