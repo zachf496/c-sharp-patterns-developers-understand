0eef087f8ca5122b12818ed07f9727c8ca5da3c4
Zachary <zacharyf496@gmail.com>
2016-03-10 13:55:51 -0500

    Add some singleton notes

--- Singleton/MySingleton.cs
+++ Singleton/MySingleton.cs
@@ -7,22 +7,28 @@ namespace Singleton
         private static volatile MySingleton _instance;
         private static object _padLock = new Object();
 
+        //you can create as many properties as you'd like
         public string PropertyOne;
-
+        
+        //note that the constructor is private and only the class itself can create a new instance
         private MySingleton()
         {
         }
 
+        //this is where the instance will live, notice the return value is the singleton class itself
         public static MySingleton Instance
         {
             get
             {
+                //using double check locking to make this thread-safe
+                //if already initialized, it'll just return the already created instance
                 if (_instance == null)
                 {
                     lock (_padLock)
                     {
                         if (_instance == null)
                         {
+                            //this will only run if the singleton has never been initialized or has been set to null
                             Console.WriteLine("Initializing singleton...");
 
                             _instance = new MySingleton();
@@ -36,6 +42,7 @@ namespace Singleton
             }
         }
 
+        //a helper method designed to set the singleton to null which triggers a rebuilding of the instance on next use
         public void Clear()
         {
             Console.WriteLine("Clearing singleton...");
