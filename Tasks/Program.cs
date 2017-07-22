d696f6b148c4dd5cbe84787e9babc2a88d265a80
Zachary <zacharyf496@gmail.com>
2017-07-22 16:26:42 -0400

    Add basic tasks

---
+++ Tasks/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tasks.Work;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var work = new DoSomething();
+
+            //immediately runs in a separate thread
+            var process1Task = Task.Run(() => work.Process1());
+
+            //immediately runs in a separate thread
+            var process2Task = Task.FromResult(work.Process2());
+
+            //blocks until it's done due to the 'Result' keyword
+            Console.WriteLine($"Status: {process2Task.Result}");
+
+            //let's kick off process3 immediately
+            var process3Task = _process3Async(work);
+
+            //and wait for it (meanwhile process 1 is still going likely)
+            var process3TaskResult = process3Task.Result;
+
+            Console.WriteLine($"Process3: {process3TaskResult}");
+
+            //if we don't wait, the main thread will exit before Process1 is complete.
+            //we'll never risk this with Process2 since we block until it's done
+            process1Task.Wait();
+
+            Console.WriteLine("All tasks complete!");
+
+            Console.ReadKey();
+
+            //now let's run three tasks but wait for them all to complete
+            //they will end in reverse order
+            Task.WaitAll(new Task[]
+            {
+                work.Process3Async(3000),
+                work.Process3Async(2000),
+                work.Process3Async(1000)
+            });
+
+            Console.WriteLine("All tasks complete!");
+
+            Console.ReadKey();
+
+            //now let's run three tasks but wait for ONLY ONE of them to complete
+            //they will end in reverse order
+            Task.WaitAny(new Task[]
+            {
+                work.Process3Async(3000),
+                work.Process3Async(2000),
+                work.Process3Async(1000)
+            });
+
+            Console.WriteLine("One task complete!");
+           
+            Console.ReadKey();
+        }
+
+        //you can define an async task by using the 'async' keyword, the 'Task<TResult>` return type and the 'await keyword on something
+        //defined as a task
+        private static async Task<string> _process3Async(DoSomething work)
+        {
+            return await work.Process3Async();
+        }
+    }
+}
---
+++ Tasks/Work/DoSomething.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks.Work
+{
+    public class DoSomething
+    {
+        public void Process1()
+        {
+            Console.WriteLine("Doing Process1....");
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("Process1 Done.");
+        }
+
+        public string Process2()
+        {
+            Console.WriteLine("Doing Process2....");
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Process2 Done.");
+
+            return "All done.";
+        }
+
+        public async Task<string> Process3Async(int sleepInMs = 5000)
+        {
+            //should run immediately
+            Console.WriteLine($"Doing Process3 ({sleepInMs})...");
+
+            //simulates a blocking task
+            await Task.Run(() =>
+            {
+                Thread.Sleep(sleepInMs);
+            });
+
+            //this should run afterwards
+            Console.WriteLine($"Process3 ({sleepInMs}) Done.");
+
+            return "All done.";
+        }
+    }
+}
